dfce9bb5ca48a4308ef0e630681f3be690b50bea
Matthew Brown <mbrown@geomarvel.com>
2022-01-07 19:23:12 -0500

    Initial Commit

---
+++ GoalsApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GoalsApp
+//
+//  Created by Matt Brown on 1/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GoalsApp/GoalsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoalsAppApp.swift
+//  GoalsApp
+//
+//  Created by Matt Brown on 1/7/22.
+//
+
+import SwiftUI
+
+@main
+struct GoalsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
